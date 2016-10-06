85126fc0e4f28b55cabd4f7466624702ac6061fd
Jesper Mikkelsen Post <jesp6544@edu.eal.dk>
2016-10-06 21:12:34 +0200

    Added first step with json

---
+++ JSON test/JSON test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace JSON_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.hk = 10;
+            car.bla = 1000;
+            car.text = "asdflkasdfdsfg";
+
+            Car[] carAr = new Car[2];
+            carAr[0] = car;
+            carAr[1] = new Car(){ hk = 20, bla = 2, text = "nr2"};
+            
+                string test = JsonConvert.SerializeObject(carAr, Formatting.Indented);
+            dynamic testing = JsonConvert.DeserializeObject(test);
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"config.JSON",test);
+            //Console.WriteLine(testing.hk);
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public int hk { get; set; }
+        public int bla { get; set; }
+        public string text { get; set; }
+    }
+}
